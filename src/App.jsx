374b5b54d640c0fd897c7a598093ca7180f5099c
Ebdollah <l201165@lhr.nu.edu.pk>
2024-07-25 12:56:41 +0500

    wrote bit complex form fucntionality

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Children from "./Learning/Children/Children";
+import Children1 from "./Learning/Children/Children1";
+import Children2 from "./Learning/Children/Children2";
+import L1 from "./Learning/Lists/L1";
+import L2 from "./Learning/Lists/L2";
+import E1 from "./Learning/Events/E1";
+import E2 from "./Learning/Events/E2";
+import F1 from "./Learning/Forms/F1";
+
+
+
+
+
+
+
+export default function App() {
+  return (
+    <>
+      <div>
+        {/* <Children /> */}
+        {/* <Children1 /> */}
+        {/* <Children2 /> */}
+        {/* <L1 /> */}
+        {/* <L2 /> */}
+        {/* <E1 /> */}
+        {/* <E2 /> */}
+        <F1/>
+      </div>
+    </>
+  )
+}
