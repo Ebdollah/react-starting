0e9d2db184aa1c683b72cd1270ded1a754d03bbe
Ebdollah <l201165@lhr.nu.edu.pk>
2024-07-29 12:16:07 +0500

    worked with events and states

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import L2 from "./Learning/Lists/L2";
 import E1 from "./Learning/Events/E1";
 import E2 from "./Learning/Events/E2";
 import F1 from "./Learning/Forms/F1";
+import S1 from "./Learning/State/S1";
+import S2 from "./Learning/State/S2";
+import E3 from "./Learning/Events/E3";
+import E4 from "./Learning/Events/E4";
+import S3 from "./Learning/State/S3";
 
 
 
@@ -24,7 +29,12 @@ export default function App() {
         {/* <L2 /> */}
         {/* <E1 /> */}
         {/* <E2 /> */}
-        <F1/>
+        {/* <F1/> */}
+        {/* <S1 /> */}
+        {/* <S2 /> */}
+        {/* <E3 /> */}
+        {/* <E4 /> */}
+        <S3 />
       </div>
     </>
   )
